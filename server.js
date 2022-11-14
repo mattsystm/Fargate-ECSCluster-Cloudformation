324871293008b7311c9dda53ea74eb7863b32944
mattsystm <mtsahin@amazon.com>
2022-11-14 15:25:13 -0500

    new version

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World Final Version');
+  res.send('Hello World New Version');
 });
 
 app.listen(PORT, HOST);
